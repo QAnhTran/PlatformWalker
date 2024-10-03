981f3bdb955999684d1fbd1562515510f7d84b16
QAnhTran <tquangnh87@gmail.com>
2024-10-03 23:16:46 +0700

    update function to add item in inventory

--- Assets/Pixel Adventure 1/Assets/Scripts/ItemCollector.cs
+++ Assets/Pixel Adventure 1/Assets/Scripts/ItemCollector.cs
@@ -9,12 +9,27 @@ public class ItemCollector : MonoBehaviour
 
     [SerializeField] private Text cherriesText;
     [SerializeField] private AudioSource collectionSoundEffect;
+    public Sprite itemIcon;  
+    private InventoryManager inventoryManager;
+
+    private void Start()
+    {
+        inventoryManager = GameObject.FindGameObjectWithTag("Inventory Manager").GetComponent<InventoryManager>();
+    }
 
      private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Cherry"))
         {
             collectionSoundEffect.Play();
+
+        InventoryItem newItem = new InventoryItem
+        {
+            itemName = "Cherry",
+            itemIcon = itemIcon 
+        };
+
+            inventoryManager.AddItemToSlot(itemIcon, newItem);
             Destroy(collision.gameObject);
             cherries++;
             cherriesText.text = "Cherries: " + cherries;
